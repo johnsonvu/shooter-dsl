2c85aaa589eb4b5fbf09fe2d16c7e74fe30e6d78
MengXin Zhao <mengxin.zhao721@gmail.com>
2019-10-02 12:48:23 -0700

    added fields for ast (latter half)

--- src/ast/MakeStatement.java
+++ src/ast/MakeStatement.java
@@ -3,6 +3,10 @@ package ast;
 import visitor.Visitor;
 
 public class MakeStatement extends ASTNode{
+    public Number number;
+    public Type type;
+    public Identifier identifier;
+
     @Override
     public <T> T accept(Visitor<T> v) {
         return v.visit(this);
--- src/ast/ObjectDef.java
+++ src/ast/ObjectDef.java
@@ -3,6 +3,9 @@ package ast;
 import visitor.Visitor;
 
 public class ObjectDef extends ASTNode{
+    public Identifier identifier;
+    public PropertyStatemnet propertyStatemnet;
+
     @Override
     public <T> T accept(Visitor<T> v) {
         return v.visit(this);
