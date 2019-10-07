d20742abb7c053fcce4ed1c4e88107fd72be8b2c
Johnson Vu <johnsonvu96@gmail.com>
2019-10-07 15:06:13 -0700

    Add skeleton for key inputs

---
+++ src/game/controller/KeyInput.java
@@ -0,0 +1,30 @@
+package game.controller;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.logging.Handler;
+
+public class KeyInput extends KeyAdapter {
+
+    private Handler handler;
+
+    public KeyInput(Handler handler) {
+        this.handler = handler;
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int key = e.getKeyCode();
+
+        // TODO: Add key press logic
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int key = e.getKeyCode();
+
+        // TODO: Add key release logic
+        
+    }
+}
