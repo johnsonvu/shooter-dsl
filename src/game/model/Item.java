89f23decb24e18bd7b5514929eb9035effab3a09
Johnson Vu <johnsonvu96@gmail.com>
2019-10-13 22:55:16 -0700

    Add item collisions

--- src/game/model/Item.java
+++ src/game/model/Item.java
@@ -2,6 +2,7 @@ package game.model;
 
 import game.view.Game;
 import evaluate.protoypes.ItemProto;
+import ui.Main;
 
 import java.awt.*;
 
@@ -13,7 +14,21 @@ public class Item extends GameObject {
 
     @Override
     public void tick() {
+        collision();
+    }
 
+    private void collision() {
+        for (int i = 0; i < Main.gameObjects.size(); i++) {
+            GameObject obj = Main.gameObjects.get(i);
+            if (obj instanceof Player) {
+                if (this.getBounds().intersects(obj.getBounds())) {
+                    if(obj.health < obj.proto.health) {
+                        obj.health += this.health;
+                    }
+                    Main.gameObjects.remove(this);
+                }
+            }
+        }
     }
 
     @Override
@@ -21,10 +36,5 @@ public class Item extends GameObject {
         int xCenter = x - image.getWidth(null)/2;
         int yCenter = y - image.getHeight(null)/2;
         g.drawImage(image, xCenter,yCenter, null);
-    }
-
-    @Override
-    public Rectangle getBounds() {
-        return null;
     }
 }
